af219506ca2e2a9dd88f4516edca84f011a80b7a
IT24100162 <it24100162@my.sliit.lk>
2025-09-16 14:36:43 +0530

    IT24100162_Lab Sheet 07

---
+++ Lab Sheet 07/IT24100162.R
@@ -0,0 +1,20 @@
+setwd("C:\\Users\\rames\\OneDrive\\Desktop\\IT24100162")
+getwd()
+
+#Question 01
+
+punif(25, min = 0, max = 40) - punif(10, min = 0, max = 40)
+
+#Question 02
+
+pexp(2, rate = 1/3)
+
+#Question 03
+
+  # i)
+    pnorm(130, mean = 100, sd = 15, lower.tail = FALSE)
+    
+    
+  # ii)
+   qnorm(0.95, mean = 100, sd = 15)
+    
